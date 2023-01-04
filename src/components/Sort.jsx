dc9d8c853b504a5a58e6173d3f76f556469a8bf9
ZeBolHD <zezebolz@gmail.com>
2023-01-04 18:41:37 +0300

    lesson 5

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 export default function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+
+  const list = ["популярности", "цене", "алфавиту"];
+  const sortName = list[selected];
+
+  const onClickListItem = (index) => {
+    setSelected(index);
+    setOpen(!open);
+  };
+
   return (
-    <div class="sort">
-      <div class="sort__label">
+    <div className="sort">
+      <div className="sort__label">
         <svg
           width="10"
           height="6"
@@ -17,15 +28,25 @@ export default function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div class="sort__popup">
-        <ul>
-          <li class="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {list.map((item, index) => {
+              return (
+                <li
+                  onClick={() => onClickListItem(index)}
+                  className={selected === index ? "active" : ""}
+                  key={index}
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
